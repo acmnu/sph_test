345ef7dae8155fe8f435dbae20a1fb60d59a6311
Anton Chevychalov <pub@mnu.pp.ru>
2017-08-16 10:36:29 +0300

    Fix sidebar

--- docs/conf.py
+++ docs/conf.py
@@ -3,3 +3,12 @@ import sphinx_rtd_theme
 master_doc = 'index'
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_sidebars = {
+    '**': [
+        'about.html',
+        'navigation.html',
+        'relations.html',  # needs 'show_related': True theme option to display
+        'searchbox.html',
+        'donate.html',
+    ]
+}
